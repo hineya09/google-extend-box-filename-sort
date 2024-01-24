627b4a50cf132901ff7a4c3807f5be2a7de72453
L.I.Takumi <32321945+hineya09@users.noreply.github.com>
2024-01-24 21:38:46 +0900

    first commit

---
+++ src/content_script.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    let url = window.location.href
+    if (getParamfunction('sortColumn', url) === null) {
+        if (url.match(/\/trash/)){
+            location.replace('?sortColumn=deleted&sortDirection=DESC')
+        } else if (url.match(/\/folder/) && !url.match(/search/)){
+            location.replace('?sortColumn=name&sortDirection=ASC')
+        }
+    }
+
+    /**
+     * Get the URL parameter value
+     *
+     * @param  name {string} パラメータのキー文字列
+     * @return  url {url} 対象のURL文字列（任意）
+     */
+    function getParamfunction (name, url) {
+        if (!url) url = window.location.href
+        name = name.replace(/[\[\]]/g, "\\$&")
+        let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url)
+        if (!results) return null
+        if (!results[2]) return ''
+        return decodeURIComponent(results[2].replace(/\+/g, " "))
+    }
+});
